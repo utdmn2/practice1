e52bc2f0160e3b98a166f93cf3ccbe0865a95bf8
utdmn2 <utdmn2@gmail.com>
2021-10-27 01:01:54 +0000

    [Add]model.contoroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'homes/top'
+  get 'homes/about'
+  get 'books/new'
+  get 'books/create'
+  get 'books/index'
+  get 'books/edit'
+  get 'books/update'
+  get 'books/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
